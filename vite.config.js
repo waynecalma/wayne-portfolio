6256c8a505c06b7a9cddb68cf79e6707bd122a2c
Gerwayne Calma <gerwaynecalma2@gmail.com>
2026-02-16 23:54:10 +0800

    Third Commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: "/wayne-portfolio/", // <--- THIS MUST MATCH YOUR GITHUB REPO NAME EXACTLY
 })
